bb70864f47d22ecbd3dc0e1ad01ccf7c8c881a7d
Yexibel <yexibelperez.qa@gmail.com>
2025-03-15 21:06:22 -0400

    Change new test

---
+++ cypress/e2e/1_secciones_1-6/hola_mundo5.cy.js
@@ -0,0 +1,32 @@
+///<reference types="cypress"/>
+
+
+describe("Bienvenido al curso de cypress sección 1", () => {
+    
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+      })
+    
+    it('Mi primer test => Hola mundo', () => {
+        cy.log("Hola mundo!");
+        cy.wait(4000)
+    })
+
+    it('Segundo test -> campo name', () => {
+        cy.visit("https://demoqa.com/text-box")
+
+        cy.get("#userName").type("Yexibel")
+        cy.wait(4000)
+    })
+
+
+    it('Tercer test -> campo name', () => {
+        cy.visit("https://demoqa.com/text-box")
+
+        cy.get("#userName").type("Yexibel")
+        cy.wait(4000)
+    })
+
+}) // Cierre del describe
