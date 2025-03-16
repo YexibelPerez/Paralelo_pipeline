e9b7c0dce9563e61f34e1c79fcf9c04a017f4097
Yexibel <yexibelperez.qa@gmail.com>
2025-03-15 20:24:56 -0400

    Initializing parallel project

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1500,
+  viewportHeight: 900,
+  chromeWebSecurity: false,
+  projectId: "hagsay",
+  video: true,
+  e2e: {
+    //para que solo se muestren archivos feature/cucumber
+    //specPattern: "**/*.feature",
+    setupNodeEvents(on, config) {
+    },
+    //para que se pueda utilizar el before en todos los it y que la pagina no se quede en blanco
+    testIsolation: false,
+  },
+
+  pageLoadTimeout: 9000,
+  defaultCommandTimeout: 15000,
+});
+
