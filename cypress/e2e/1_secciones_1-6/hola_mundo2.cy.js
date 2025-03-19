cefc5185fb96f6465f75d38b23fc320c9939fc55
Yexibel <yexibelperez.qa@gmail.com>
2025-03-18 21:08:56 -0400

    add wait time 6000

--- cypress/e2e/1_secciones_1-6/hola_mundo2.cy.js
+++ cypress/e2e/1_secciones_1-6/hola_mundo2.cy.js
@@ -1,28 +1,28 @@
 ///<reference types="cypress"/>
 
 describe("Bienvenido al curso de cypress sección 1", () => {
-  Cypress.on("uncaught:exception", (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-  });
-
-  it("Mi primer test => Hola mundo", () => {
-    cy.log("Hola mundo!");
-    cy.wait(6000);
-  });
-
-  it("Segundo test -> campo name", () => {
-    cy.visit("https://demoqa.com/text-box");
-
-    cy.get("#userName").type("Yexibel");
-    cy.wait(6000);
-  });
-
-  it("Tercer test -> campo name", () => {
-    cy.visit("https://demoqa.com/text-box");
-
-    cy.get("#userName").type("Yexibel");
-    cy.wait(6000);
-  });
-}); // Cierre del describe
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false;
+    });
+  
+    it("Mi primer test => Hola mundo", () => {
+      cy.log("Hola mundo!");
+      cy.wait(6000);
+    });
+  
+    it("Segundo test -> campo name", () => {
+      cy.visit("https://demoqa.com/text-box");
+      cy.wait(6000);
+      cy.get("#userName").type("Yexibel");
+      cy.wait(6000);
+    });
+  
+    it("Tercer test -> campo name", () => {
+      cy.visit("https://demoqa.com/text-box");
+      cy.wait(6000);
+      cy.get("#userName").type("Yexibel");
+      cy.wait(6000);
+    });
+  }); // Cierre del describe
